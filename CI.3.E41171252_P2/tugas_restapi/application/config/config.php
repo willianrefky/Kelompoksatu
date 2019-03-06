530f5e7d4ebe8b90ef7f7f3f68dbb11fae928d39
NikwaF <nikonabilah@gmail.com>
2019-03-07 00:26:18 +0700

    adding htaccess

--- CI.3.E41171252_P2/tugas_restapi/application/config/config.php
+++ CI.3.E41171252_P2/tugas_restapi/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/CI.3.E41171252_P2/tugas_restapi';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
